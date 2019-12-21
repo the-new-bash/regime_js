81a9723a2677a1dc744af20d94b675111037e261
Nikki <nicolerenvoize@hotmail.com>
2019-12-21 21:38:34 +1100

    New: added cli function to get all products

--- CONSTANTS.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-BANK_ENDPOINTS: [{ANZ:''},{CBA:''},{NAB:''},{WBC:''}]
-
-}
--- cli.js
+++ cli.js
@@ -1,12 +1,14 @@
-#!/usr/bin/env node 
+#!/usr/bin/env node
 'use strict'
 
 const program = require('commander')
+const getProducts = require('./lib/get-products.js')
 
 program
-    .command("hello")
-    .action(()=>{
-        console.log("Hello, World!")
+    .command("getAllProducts <bank>")
+    .description("gets all products for a given bank")
+    .action((bank) => {
+        getProducts.printProducts(bank)
     })
 
 program.parse(process.argv);

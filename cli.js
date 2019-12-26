abdc1457fad54804c7092d9fb8dd130a15ed0671
Nikki <nicolerenvoize@hotmail.com>
2019-12-26 15:59:27 +1100

    Update: changed return of cli function

--- cli.js
+++ cli.js
@@ -4,11 +4,12 @@
 const program = require('commander')
 const getProducts = require('./lib/get-products.js')
 
+
 program
     .command("getAllProducts <bank>")
     .description("gets all products for a given bank")
     .action((bank) => {
-        getProducts.printProducts(bank)
+        return getProducts.getAllProducts(bank)
     })
 
 program.parse(process.argv);
